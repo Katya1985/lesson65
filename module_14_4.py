a9c66b769034eda5a8cf77adaee739c9fb9fd22f
Katya1985 <berkat85@rambler.ru>
2025-01-15 13:55:59 +0300

    Update module_14_4.py

--- module_14_4.py
+++ module_14_4.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = "7322813800:AAE2uHR4DTQxxAGeQEfwjGTaNrmvbmzI8YQ"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
